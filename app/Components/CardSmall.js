206de22417e2b2038b6a88cd8861fd82efebc239
Suranjana Roy <roysupra75@gmail.com>
2024-12-09 02:23:25 +0530

    added small card component in search suggestions

---
+++ app/Components/CardSmall.js
@@ -0,0 +1,19 @@
+import React from "react";
+import Image from "next/image";
+function CardSmall({ photo, title, subtitle, price, quantity, altText }) {
+  return (
+    <div >
+        <div className="h-[9rem] w-[9rem] border-[#c0c0c0] border-[0.05rem]">
+            <Image src={photo} alt={altText}></Image>
+        </div>
+      <div>
+        <p className="text-[0.6rem] my-1 text-slate-400">{title}</p>
+        <p className="text-[0.8rem]">{subtitle}</p>
+        <span className="font-bold text-[0.91rem]">{price}</span>
+        <span className="text-[0.91rem] text-slate-400"> / {quantity} pièces</span>
+      </div>
+    </div>
+  );
+}
+
+export default CardSmall;
--- app/Components/Search.js
+++ app/Components/Search.js
@@ -1,11 +1,60 @@
 "use client";
 import React, { useState } from "react";
+import Image from "next/image";
 import { CiSearch } from "react-icons/ci";
+import {
+  IoReloadOutline,
+  IoSearch,
+  IoGridOutline,
+  IoPricetagOutline,
+} from "react-icons/io5";
+import { GoArrowUpLeft } from "react-icons/go";
+import plate from "@/public/plate.jpg";
+import cutlery from "@/public/cutlery.webp";
+import fork from "@/public/fork.webp";
+import table from "@/public/table.jpeg";
+import CardSmall from "./CardSmall";
 
 function Search() {
   const [isSearch, setIsSearch] = useState(false);
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const suggestions = [
+    {
+      label: "fourchettes",
+      startIcon: <IoReloadOutline />,
+      endIcon: <GoArrowUpLeft />,
+    },
+    {
+      label: "fourchette grande",
+      startIcon: <IoSearch />,
+      endIcon: <GoArrowUpLeft />,
+    },
+    {
+      label: "fourchette Piranha",
+      startIcon: <IoSearch />,
+      endIcon: <GoArrowUpLeft />,
+    },
+    {
+      label: "fourchette",
+      startIcon: <IoSearch />,
+      endIcon: <GoArrowUpLeft />,
+    },
+    {
+      label: "Fourchettes",
+      startIcon: <IoGridOutline />,
+      subText: "ART DE LA TABLE > FOURCHETTES",
+    },
+    { label: "Fourchette", startIcon: <IoPricetagOutline /> },
+    { label: "Sport addict", startIcon: <IoPricetagOutline /> },
+  ];
+
+  const filteredItem = suggestions.filter((item) =>
+    item.label.toLowerCase().includes(searchTerm.toLowerCase())
+  );
 
   return (
+    // search bar
     <div
       className={`bg-[#f9fafb] flex items-center rounded-md ${
         isSearch
@@ -15,6 +64,10 @@ function Search() {
     >
       <input
         type="text"
+        value={searchTerm}
+        onChange={(e) => {
+          setSearchTerm(e.target.value);
+        }}
         className="h-[50px] w-[768px] pl-[15px] bg-[#f9fafb] focus:outline-none"
         placeholder="Rechercher un produit"
         onFocus={() => {
@@ -24,14 +77,91 @@ function Search() {
           setIsSearch(false);
         }}
       />
-      <div  className={` p-4 ${
-            isSearch ? "bg-[#0093D0] text-white" : "bg-[#f9fafb] text-slate-500"
-          } hover:cursor-pointer`}>
-        <CiSearch
-          size={25}
-          className="font-bold"
-        />
+      <div
+        className={` p-4 ${
+          isSearch ? "bg-[#0093D0] text-white" : "bg-[#f9fafb] text-slate-500"
+        } hover:cursor-pointer`}
+      >
+        <CiSearch size={25} className="font-bold" />
       </div>
+
+      {/* search suggestions dropdown list w-[51.7rem]*/}
+
+      {isSearch && searchTerm && (
+        <div className="flex gap-4 absolute top-[90px]   h-auto border border-slate-300 shadow-md bg-white p-4 rounded-md">
+          <div>
+            <p className="text-sm text-slate-400 mb-4 ">Suggestions</p>
+            <div>
+              {filteredItem.length > 0 ? (
+                <>
+                  {filteredItem.map((item, index) => (
+                    <p
+                      className="flex items-center justify-between mb-4 hover:cursor-pointer"
+                      key={index}
+                    >
+                      <div className="flex gap-2">
+                        {item.startIcon && (
+                          <span className="flex items-center justify-center">
+                            {item.startIcon}
+                          </span>
+                        )}
+                        <span className="font-bold">{item.label}</span>
+                      </div>
+                      {item.subText && (
+                        <p className="relative top-4 right-[5.7rem] text-[0.6rem] text-slate-400">
+                          {" "}
+                          {item.subText}
+                        </p>
+                      )}
+                      {item.endIcon && <span>{item.endIcon}</span>}
+                    </p>
+                  ))}
+                </>
+              ) : (
+                <></>
+              )}
+            </div>
+          </div>
+          <div className="mx-4 flex flex-col">
+            <p className="text-sm text-slate-400 mb-4 ">Produits</p>
+            <div className="flex gap-6">
+              <CardSmall
+                photo={table}
+                title={"ART DE LA TABLE"}
+                subtitle={"Produit Simple"}
+                price={"7€"}
+                quantity={20}
+                altText={"table-img"}
+              />
+              <CardSmall
+                photo={plate}
+                title={"ART DE LA TABLE"}
+                subtitle={"Produit Gamme"}
+                price={"2,20€"}
+                quantity={20}
+                altText={"plate-img"}
+              />
+              <CardSmall
+                photo={cutlery}
+                title={"ART DE LA TABLE"}
+                subtitle={"Jewel Grand Couteau"}
+                price={"7€"}
+                quantity={20}
+                altText={"cutlery-img"}
+              />
+              <CardSmall
+                photo={fork}
+                title={"ART DE LA TABLE"}
+                subtitle={"Piranha Fourchette à Zakouski"}
+                price={"220€"}
+                quantity={20}
+                altText={"fork-img"}
+              />
+            </div>
+            <div className="w-full px-4 py-2 border border-[#c0c0c0] text-center mt-8 mb-2 rounded-sm">Voir tous les résultats</div>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
